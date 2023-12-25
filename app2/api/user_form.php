a39e77ebc7ba0e48670654577d4bab95125e135c
ren1023 <renyou.1023@gmail.com>
2023-12-25 12:11:05 +0800

    使用id做為判斷依據,讓一個user_form.php可以同時處理新增與修改

--- app2/api/user_form.php
+++ app2/api/user_form.php
@@ -3,30 +3,40 @@
         <div class="modal-dialog modal-dialog-centered modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="studentFormLabel">新增學生</h1>
+                    <h1 class="modal-title fs-5" id="studentFormLabel"> 
+                        <?=(isset($_GET['id']))?'編輯學生':'新增學生';?>  <!-- 因為新增和編輯的功能放在同的個form處理，所以要多加這個判斷式 -->
+                    </h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
                     <form action="./api/insert.php" method="post" class="row p-3">
+                    <?php
+                    if(isset($_GET['id'])){  /* 只要有id的欄位，就執行以下動作 */
+                        include_once "db.php";
+                       $user=$Student->find($_GET['id']); /*  用id去資料庫撈學生的資料 */
+                       extract($user);
+                    }
+                ?>
+
                         <div class="mb-3 col-6">
                             <label for="name" class="form-label">姓名</label>
-                            <input type="text" class="form-control" name="name" id="name">
+                            <input type="text" class="form-control" name="name" id="name" value="<?=$name??'';?>">
                         </div>
                         <div class="mb-3 col-6">
                             <label for="school_num" class="form-label">學號</label>
-                            <input type="text" class="form-control" name="school_num" id="school_num">
+                            <input type="text" class="form-control" name="school_num" id="school_num" value="<?=$school_num??'';?>">
                         </div>
                         <div class="mb-3 col-6">
                             <label for="birthday" class="form-label">生日</label>
-                            <input type="date" class="form-control" name="birthday" id="birthday">
+                            <input type="date" class="form-control" name="birthday" id="birthday" value="<?=$birthday??'';?>">
                         </div>
                         <div class="mb-3 col-6">
                             <label for="uni_id" class="form-label">身分證字號</label>
-                            <input type="text" class="form-control" name="uni_id" id="uni_id">
+                            <input type="text" class="form-control" name="uni_id" id="uni_id" value="<?=$uni_id??'';?>">
                         </div>
                         <div class="mb-3 col-6">
                             <label for="addr" class="form-label">地址</label>
-                            <input type="text" class="form-control" name="addr" id="addr">
+                            <input type="text" class="form-control" name="addr" id="addr" value="<?=$addr??'';?>">
                         </div>
                         <div class="mb-3 col-6">
                             <label for="parents" class="form-label">家長</label>
@@ -42,7 +52,7 @@
                         </div>
                         <div class='mb-3 col-6'>
                             <label for="schools" class="form-label">畢業學校</label>
-                            <select id="schools" name="graduate_at" class="form-select" aria-label="Default select" >
+                            <select name="graduate_at" class="form-select" aria-label="Default select" id="schools" >
                                 <!-- 製作畢業學校的選單事件 -->
                             </select>
                         </div>
@@ -51,7 +61,9 @@
                             <input type="text" class="form-control" name="status_code" id="status_code">
                         </div>
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">取消</button>
-                        <button type="submit" class="btn btn-primary">確認新增</button>
+                        <button type="submit" class="btn btn-primary">
+                        <?=(isset($_GET['id']))?'編輯更新':'確認新增';?>  <!-- 如果有id，顯示：編輯更新 ，如果沒有，就顯示 確認新增-->
+                        </button>
                     </form>
                 </div>
                 <div class="modal-footer">
